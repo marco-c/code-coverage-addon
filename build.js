da0928d1d6825d82b608371d0ba3551d0d14b526
streanga-sarmis <streangasarmis@gmail.com>
2018-02-21 19:39:04 +0200

    Made even more changes

--- build.js
+++ build.js
@@ -1,77 +1,58 @@
 "use strict";
 
 const fs = require('fs');
-const https = require('follow-redirects').https;
+const https = require('https');
 const archiver = require('archiver');
 
-let options = {
-    host: 'uplift.shipit.staging.mozilla-releng.net',
-    path: '/coverage/supported_extensions',
-    method: 'GET',
-    headers: {"Content-Type": "text/plain"}
-}
+let req = https.get('https://uplift.shipit.staging.\
+mozilla-releng.net/coverage/supported_extensions', res => {
 
-
-let req = https.get(options, (res) => {
-
-	let data = '';
+  let data = '';
     
-	res.on('data', (chunk) => {
-		data += chunk;
-	});
+  res.on('data', chunk =>	data += chunk);
 
-	res.on('end', () => {
+  res.on('end', () => {
 
-		let content = `const SUPPORTED_EXTENSIONS = ${data};`;
+    let content = `const SUPPORTED_EXTENSIONS = ${data};`;
 
-		fs.writeFile("supported_extensions.js", content, (e) => {
-    	if (e) {
-    		console.log(e.message);
-    	}
-		});
+    fs.writeFile("supported_extensions.js", content, e => {
+      if (e) {
+      	console.log(e.message);
+      }
+    });
 
-		let excludeFiles = [
-		".gitignore", ".travis.yml",
-		"LICENSE", "README.md",
-		"package.json", "package-lock.json",
-		"requirements.txt", "setup.cfg",
-		"build.js", "build.py"];
-		
+    let excludeFiles = [
+    ".gitignore", ".travis.yml",
+    "LICENSE", "README.md",
+    "package.json", "package-lock.json",
+    "requirements.txt", "setup.cfg",
+    "build.js", "build.py"];
 
-		fs.readdir(".", (e, files) => {
-			const resultFiles = files.filter(file => !excludeFiles.includes(file));
+    fs.readdir(".", (e, files) => {
+      const resultFiles = files.filter(file => !excludeFiles.includes(file));
 
-			makeZip(resultFiles);
-		});
+      makeZip(resultFiles);
+    });
 
-  });
+    });
 
-}).on('error', (e) => {
-    console.log(e.message);
-});
+}).on('error', e => console.log(e.message));
 
 function makeZip(list){
-	const output = fs.createWriteStream('./gecko-code-coverage.zip');
+  const output = fs.createWriteStream('./gecko-code-coverage.zip');
 
-	let archive = archiver('zip', {
-	    gzip: true,
-	    zlib: { level: 9 }
-	});
+  let archive = archiver('zip', {
+      gzip: true,
+      zlib: { level: 9 }
+  });
 
-	archive.on('error', (e) => {
-		if (e) {
-			console.log(e.message);
-		}
-	});
+  archive.on('error', e => console.log(e.message));
 
-	archive.pipe(output);
+  archive.pipe(output);
 
-	list.forEach (file => {
-		archive.file(file);
-	});
+  list.forEach (file => archive.file(file));
 
-	archive.finalize();
+  archive.finalize();
 }
 
 req.end();
-
