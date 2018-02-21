3b4655abc0d4a5a1ddedeab474a3545f6934dc29
streanga-sarmis <streangasarmis@gmail.com>
2018-02-21 19:51:52 +0200

    Removed/replaced a few tabs and newlines, removed the req variable

--- build.js
+++ build.js
@@ -4,37 +4,35 @@ const fs = require('fs');
 const https = require('https');
 const archiver = require('archiver');
 
-let req = https.get('https://uplift.shipit.staging.\
-mozilla-releng.net/coverage/supported_extensions', res => {
+https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions', res => {
 
   let data = '';
     
-  res.on('data', chunk =>	data += chunk);
-
+  res.on('data', chunk => data += chunk);
   res.on('end', () => {
-
     let content = `const SUPPORTED_EXTENSIONS = ${data};`;
 
     fs.writeFile("supported_extensions.js", content, e => {
       if (e) {
-      	console.log(e.message);
+        console.log(e.message);
       }
     });
 
     let excludeFiles = [
-    ".gitignore", ".travis.yml",
-    "LICENSE", "README.md",
-    "package.json", "package-lock.json",
-    "requirements.txt", "setup.cfg",
-    "build.js", "build.py"];
+      ".gitignore", ".travis.yml", "LICENSE", "README.md",
+      "package.json", "package-lock.json", "build.js"
+    ];
 
     fs.readdir(".", (e, files) => {
+      if(e){
+        console.log(e.message);
+      }
       const resultFiles = files.filter(file => !excludeFiles.includes(file));
 
       makeZip(resultFiles);
     });
 
-    });
+  });
 
 }).on('error', e => console.log(e.message));
 
@@ -42,7 +40,6 @@ function makeZip(list){
   const output = fs.createWriteStream('./gecko-code-coverage.zip');
 
   let archive = archiver('zip', {
-      gzip: true,
       zlib: { level: 9 }
   });
 
@@ -50,9 +47,7 @@ function makeZip(list){
 
   archive.pipe(output);
 
-  list.forEach (file => archive.file(file));
+  list.forEach(file => archive.file(file));
 
   archive.finalize();
 }
-
-req.end();
