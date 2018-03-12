474f34ce5bbb271fc7a848eec04980f04ac72b18
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-12 16:42:15 +0600

    Update build.js

--- build.js
+++ build.js
@@ -11,7 +11,20 @@ const includeFiles = [
   'spinner.css', 'supported_extensions.js',
 ];
 const zipName = './gecko-code-coverage.zip';
-
+var manObj;
+fs.readFile('manifest.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  manObj = JSON.parse(data);
+});
+var packObj;
+fs.readFile('package.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  packObj = JSON.parse(data);
+});
+if (manObj["version"] != packObj["version"]) {
+     console.error("Different versions of manifest.json and package.json");
+     return;
+}
 https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions', res => {
   let data = '';
     
@@ -24,7 +37,7 @@ https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_e
         console.error(e.message);
         return;
       }
-
+    
       fs.readdir('.', (e, files) => {
         if (e) {
           console.error(e.message);
