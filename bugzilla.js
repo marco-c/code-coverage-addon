8de44f257de0ce95cad4ef4349f4f2cab96a7253
Divya Baid <32747809+divyabaid16@users.noreply.github.com>
2018-03-19 01:38:27 +0530

    coverage information in the uplift request comments
    Fix [#10](https://github.com/marco-c/code-coverage-addon/issues/10)

--- bugzilla.js
+++ bugzilla.js
@@ -8,7 +8,26 @@
   const container = document.getElementById('module-details-content');
   const field_has_str = document.getElementById('field_label_cf_has_str');
 
+  var uplift;
+  var j=0;
+  var myString;
+  var element =  document.getElementById('ct-${j}');
+  var str = "Is this code covered by automated tests?";
+  while (typeof(element) != 'undefined' && element != null){
+
+  		var i;
+  		for(i=0;i<element.length;i++){
+  				myString = element.innerHTML;
+  				if(myString==str){
+  						uplift=element;
+  				}
+  		}
+
+  		element =  document.getElementById('ct-j');
+ }
+  
   const bugzilla_modal_ui = !!container;
+  const bugzilla_modal_ui_1 = !!element;
 
   const hgurlPattern = new RegExp('^http[s]?://hg\\.mozilla\\.org/mozilla-central/rev/([0-9a-f]+)$');
   const revs = [];
@@ -57,10 +76,11 @@
     a.className = 'help';
     a.textContent = 'Code Coverage:';
     a.href = 'https://addons.mozilla.org/firefox/addon/gecko-code-coverage/';
-    nameDiv.append(a);
+	  nameDiv.append(a);
     mainDiv.appendChild(nameDiv);
     mainDiv.appendChild(valueDiv);
     container.appendChild(mainDiv);
+    
   } else {
     const tr = document.createElement('tr');
     const th = document.createElement('th');
@@ -78,6 +98,72 @@
     td.append(valueDiv);
     tr.append(td);
     field_has_str.parentNode.parentNode.insertBefore(tr, field_has_str.parentNode.nextSibling);
+  }
+
+  sssss    const selector_up = bugzilla_modal_ui ? '.comment-text > a' : '.bz_comment_text > a';
+  document.querySelectorAll(selector_up).forEach(a => {
+    const parentId_up = a.parentNode.attributes.id;
+    if (parentId_up !== currentCommentId) {
+    // we're in a new comment
+      currentCommentId = parentId_up;
+      isFirst = false;
+    }
+    const prev_up = a.previousSibling;
+    if (prev_up == null || (prev_up.previousSibling == null && !prev_up.textContent.trim())) {
+    // the first element in the comment is the link (no text before)
+      isFirst = true;
+    }
+    if (isFirst) {
+    // so we take the first link and the following ones only if they match the pattern
+      const link_up = a.href;
+      const m_up = link_up.match(hgurlPattern);
+      if (m_up != null) {
+        let rev = m[1];
+        if (rev.length > 12) {
+          rev = rev.slice(0, 12);
+        }
+        revs.push(rev);
+      }
+    }
+  });
+
+  if (revs.length == 0) {
+    return;
+  }
+
+  
+
+  if (bugzilla_modal_ui_1) {
+    const mainDiv_up = document.createElement('div');
+    mainDiv_up.className = 'field';
+    const nameDiv_up = document.createElement('div');
+    nameDiv_up.className = 'name';
+    const a_up = document.createElement('a');
+    a_up.className = 'help';
+    a_up.textContent = 'Code Coverage:';
+    a_up.href = 'https://addons.mozilla.org/firefox/addon/gecko-code-coverage/';
+	nameDiv_up.append(a_up);
+    mainDiv_up.appendChild(nameDiv_up);
+    mainDiv_up.appendChild(valueDiv_up);
+    element.appendChild(mainDiv_up);
+    
+  } else {
+    const tr_up = document.createElement('tr');
+    const th_up = document.createElement('th');
+    th_up.setAttribute('class', 'field_label');
+    tr_up.append(th);
+    const a_up = document.createElement('a');
+    a_up.className = 'field_help_link';
+    a_up.title = 'Code coverage for landed patches';
+    a_up.href = 'https://addons.mozilla.org/firefox/addon/gecko-code-coverage/';
+    a_up.textContent = 'Code Coverage:';
+    th_up.append(a_up);
+    const td = document.createElement('td');
+    td_up.setAttribute('class', 'field_value');
+    td_up.setAttribute('colspan', 2);
+    td_up.append(valueDiv);
+    tr_up.append(td_up);
+    uplift.parentNode.parentNode.insertBefore(tr, uplift.parentNode.nextSibling);
   }
 
   let promises = revs.map(fetchChangesetCoverage);
